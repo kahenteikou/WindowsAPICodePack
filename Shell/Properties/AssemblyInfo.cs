a1db800af2e7d0d58cdd3c661193d5f22c7675a7
kokkiemouse <kokkiemouse@gmail.com>
2020-04-10 07:39:09 +0900

    1st

---
+++ Shell/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿//Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.WindowsAPICodePack.Shell")]
+[assembly: AssemblyDescription("WindowsAPICodePack for Shell")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Microsoft Windows API Code Pack for .NET Framework")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: InternalsVisibleTo("Microsoft.WindowsAPICodePack.ShellExtensions,PublicKey=00240000048000009400000006020000002400005253413100040000010001002188e05ffb7992b9f8689389c32b987bb26e4c20a91b0d1585194ae0cb39f75e9c23cfa91f7a64dd34cf54464769e8786e1f2dbd7554a530bd56d86f7f9ac08af0ad1d4ef1d92f9605668e01b599252b67da70546791f4232e249ef167dbdf2355408a9acc16b61ad584b949f05b2e444f10ebdda465a63c447a02176d0798c7")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48fe0672-7ff4-48fd-a3fd-83c8fa8c3506")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+
+[assembly: NeutralResourcesLanguageAttribute("en")]
